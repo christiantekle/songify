b71d3a8c8d9734d7942245201924833506a9e965
christiantekle <christiantekle@users.noreply.github.com>
2020-06-17 21:50:14 +0400

    structuring the project

---
+++ src/components/Songs.js
@@ -0,0 +1,13 @@
+import React, { Components } from "react";
+
+class Songs extends Components {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Songs;
